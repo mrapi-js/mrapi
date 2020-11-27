cd08b229131bd5e15da6f3ff949db1a7d96689e4
junlincchen <junlincchen@tencent.com>
2020-11-27 09:45:48 +0800

    fix:鉴权示例修改

--- examples/service-multiple/src/post/middlewares/permissions.ts
+++ examples/service-multiple/src/post/middlewares/permissions.ts
@@ -3,13 +3,6 @@ import { Context } from '../context'
 const isAuthenticated = rule({ cache: 'contextual' })(
     async (parent:any, args:any, ctx:Context, info:any) => {
 
-        try{
-            const result = await ctx.prisma.user.findMany({})
-            console.log("----:result:",result)
-        }catch(error){
-             console.log("-----",error);
-        }
-        
       return  new Error("没有权限");// true pass
     },
   )
