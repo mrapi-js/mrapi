3a68f1fd062a7a76cd7bfb81147e529c59e2ed44
zongmingli@outlook.com <leo@LEOZMLI-MBP.local>
2020-12-29 10:48:02 +0800

    test: 添加packages/app 覆盖率测试

---
+++ packages/app/tests/index.test.ts
@@ -0,0 +1,102 @@
+import path from 'path'
+import { App } from '../src/index'
+import http from 'http'
+const configPath = path.join(__dirname, '__fixtures__/config/mrapi.config.js')
+const config = require(configPath)
+let apptest: App = new App(config)
+
+describe('index', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  afterAll(() => {
+    apptest.close()
+  })
+
+  test('app', () => {
+    expect(typeof apptest).toBe('object')
+  })
+
+  test('app option', () => {
+    expect(typeof apptest.defaultRoute).toBe('function')
+    expect(typeof apptest.errorHandler).toBe('function')
+    expect(typeof apptest.logger.level).toBe('string')
+    expect(typeof apptest.parse).toBe('function')
+    expect(apptest.routes instanceof Array).toBe(true)
+  })
+
+  test('app lookup', async () => {
+    expect(typeof apptest.lookup).toBe('function')
+    const subApp = new App(config)
+    subApp.get('/foo', (req, res) => {
+      res.end(`hello from foo! ${req.query ? 11 : 22}`)
+    })
+    apptest.use('/v1', subApp).listen(3030, () => {})
+    function httpReq () {
+      return new Promise(resolve => {
+        http.get('http://127.0.0.1:3030/v1/foo', res => {
+          let body = ''
+          res.on('data', data => {
+            body += data
+          })
+          res.on('end', () => {
+            resolve(body)
+          })
+        })
+      })
+    }
+    const body = await httpReq()
+    expect(body).toBe('hello from foo! 11')
+    // listen
+    const apphttp2 = new App({ http2: true })
+    expect(() => {
+      apphttp2.listen(3031, { http2: true })
+    }).toThrow()
+
+    const apptest2 = new App({
+      http2: true,
+      https: {},
+    })
+    apptest2.listen(3032)
+    // close
+    apptest2.close()
+  })
+
+  test('close error', async () => {
+    const apptest2 = new App({
+      http2: true,
+      https: {},
+    })
+    apptest2.listen(3032)
+    const tmp = apptest2.server
+    apptest2.server = undefined
+    // close
+    try {
+      await apptest2.close()
+    } catch (error) {
+      expect(error).toBe('server not started')
+      apptest2.server = tmp
+      apptest2.close()
+    }
+  })
+
+  // test('close error with server exist', async () => {
+  //   const apptest2 = new App({
+  //     http2: true,
+  //     https: {},
+  //   })
+  //   apptest2.listen(3032)
+  //   const s = createServer()
+  //   s.close = function () {
+  //     throw Error('server close test')
+  //   }
+  //   apptest2.server = s
+
+  //   try {
+  //     await apptest2.close()
+  //   } catch (error) {
+  //     expect(error.message).toBe('server close test')
+  //   }
+  // })
+})
---
+++ packages/app/tests/res.test.ts
@@ -0,0 +1,161 @@
+import path from 'path'
+import { App } from '../src/index'
+import http from 'http'
+import { send } from '../src/res'
+
+const configPath = path.join(__dirname, '__fixtures__/config/mrapi.config.js')
+const config = require(configPath)
+let apptest: App = new App(config)
+
+describe('res', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  afterAll(() => {
+    apptest.close()
+  })
+
+  test('case string', async () => {
+    apptest.listen(3030)
+    apptest.get('/foo', (req, res) => {
+      send(req, res, 'foo')
+    })
+    apptest.get('/foo2', (req, res) => {
+      res.status(204)
+      send(req, res)
+    })
+    apptest.head('/head', (req, res) => {
+      send(req, res)
+    })
+    apptest.get('/user', (req, res) => {
+      send(req, res, { name: 'leo' })
+    })
+    apptest.get('/user2', (req, res) => {
+      send(req, res, null)
+    })
+    apptest.get('/buffer', (req, res) => {
+      send(req, res, Buffer.from([], 10, 10))
+    })
+    // send string
+    function httpReq () {
+      return new Promise(resolve => {
+        http.get('http://127.0.0.1:3030/foo', res => {
+          let body = ''
+          res.on('data', data => {
+            body += data
+          })
+          res.on('end', () => {
+            expect(body).toBe('foo')
+            resolve(body)
+          })
+        })
+      })
+    }
+    await httpReq()
+    // status code 204/304
+    function httpReq2 () {
+      return new Promise(resolve => {
+        http.get('http://127.0.0.1:3030/foo2', res => {
+          let body = ''
+          res.on('data', data => {
+            body += data
+          })
+          res.on('end', () => {
+            expect(body).toBe('')
+            resolve(body)
+          })
+        })
+      })
+    }
+    await httpReq2()
+    // // send head
+    function httpReq3 () {
+      return new Promise(resolve => {
+        http.get(
+          'http://127.0.0.1:3030/head',
+          {
+            method: 'head',
+          },
+          res => {
+            let body = ''
+            res.on('data', data => {
+              body += data
+            })
+            res.on('end', () => {
+              expect(body).toBe('')
+              resolve(body)
+            })
+          },
+        )
+      })
+    }
+    await httpReq3()
+    // data
+    function httpReq4 () {
+      return new Promise(resolve => {
+        http.get(
+          'http://127.0.0.1:3030/user',
+          {
+            method: 'get',
+          },
+          res => {
+            let body = ''
+            res.on('data', data => {
+              body += data
+            })
+            res.on('end', () => {
+              expect(body).toBe('{"name":"leo"}')
+              resolve(body)
+            })
+          },
+        )
+      })
+    }
+    await httpReq4()
+    // data null
+    function httpReq5 () {
+      return new Promise(resolve => {
+        http.get(
+          'http://127.0.0.1:3030/user2',
+          {
+            method: 'get',
+          },
+          res => {
+            let body = ''
+            res.on('data', data => {
+              body += data
+            })
+            res.on('end', () => {
+              expect(body).toBe('')
+              resolve(body)
+            })
+          },
+        )
+      })
+    }
+    await httpReq5()
+    // data buffer
+    function httpReq6 () {
+      return new Promise(resolve => {
+        http.get(
+          'http://127.0.0.1:3030/buffer',
+          {
+            method: 'get',
+          },
+          res => {
+            let body = ''
+            res.on('data', data => {
+              body += data
+            })
+            res.on('end', () => {
+              expect(body).toBe('')
+              resolve(body)
+            })
+          },
+        )
+      })
+    }
+    await httpReq6()
+  })
+})
