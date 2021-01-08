50f8ba6776cb17bdd2b3d372b5ce7bc6b6c338d4
Cyrusyjli <42905274@qq.com>
2021-01-08 17:24:20 +0800

    feat: service

---
+++ packages/service/tests/queries/fetchArrayData.js
@@ -0,0 +1,29 @@
+const {
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql')
+
+module.exports = {
+  type: new GraphQLList(GraphQLString),
+  description: 'array类型数据例子',
+  args: {
+    text: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: '内容'
+    }
+  },
+  resolve: (root, params, context) => {
+    const { text } = params
+
+    return [text]
+  }
+}
+
+
+// 调用例子
+// query fetchArrayData {
+//   fetchArrayData(
+//     text: "hahaha"
+//   )
+// }
---
+++ packages/service/tests/shema.js
@@ -0,0 +1,33 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType
+} = require('graphql')
+
+const { AnimalType, dogType, fishType } = require("./types/interfaceType")
+
+// 规范写法，声明query(查询类型接口) 和 mutation(修改类型接口)
+module.exports = new GraphQLSchema({
+  types: [AnimalType, dogType, fishType],
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: '查询数据',
+    fields: () => ({
+      // 查询类型接口方法名称
+      fetchObjectData: require('./queries/fetchObjectData'),
+      fetchArrayData: require('./queries/fetchArrayData'),
+      fetchListData: require('./queries/fetchListData'),
+      fetchEnumData: require('./queries/fetchEnumData'),
+      fetchInputObjectData: require('./queries/fetchInputObjectData'),
+      fetchUnionData: require('./queries/fetchUnionData'),
+      fetchInterfaceData: require('./queries/fetchInterfaceData')
+    })
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    description: '修改数据',
+    fields: () => ({
+      // 修改类型接口方法名称
+      updateData: require('./mutations/updateData')
+    })
+  })
+})
